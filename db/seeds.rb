7bee7207d1bfc4602e80e29ce75e6304624d3daf
Nicolo Buzzi <nicolo@buzzi.org>
2019-08-19 16:28:29 +0200

    change time value in schedule

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,13 @@ kebab_shop.save!
 
 Review.new(kebab_shop_id: kebab_shop.id, content:"content test", rating: 5).save!
 
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "monday",    opening_hour: 100000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "tuesday",   opening_hour: 100000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "wednesday", opening_hour: 100000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "thursday",  opening_hour: 100000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "friday",    opening_hour: 100000,  closing_hour: 50000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "saturday",  opening_hour: 100000,  closing_hour: 50000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "sunday",    opening_hour: 100000,  closing_hour: 30000 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "monday",    opening_hour: 10,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "tuesday",   opening_hour: 10,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "wednesday", opening_hour: 10,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "thursday",  opening_hour: 10,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "friday",    opening_hour: 10,  closing_hour: 5 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "saturday",  opening_hour: 10,  closing_hour: 5 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "sunday",    opening_hour: 10,  closing_hour: 3 ).save!
 puts "Kebab Shop 1 created!"
 
 puts "Creating Kebab Shop 2"
@@ -27,13 +27,13 @@ puts "creating review kebab 2"
 Review.new(kebab_shop_id: kebab_shop.id, content:"content test", rating: 3).save!
 
 puts "Creating schedule kebab Shop 2"
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "monday",    opening_hour: 90000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "tuesday",   opening_hour: 90000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "wednesday", opening_hour: 90000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "thursday",  opening_hour: 90000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "friday",    opening_hour: 120000,  closing_hour: 50000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "saturday",  opening_hour: 120000,  closing_hour: 20000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "sunday",    opening_hour: 120000,  closing_hour: 20000 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "monday",    opening_hour: 9,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "tuesday",   opening_hour: 9,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "wednesday", opening_hour: 9,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "thursday",  opening_hour: 9,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "friday",    opening_hour: 12,  closing_hour: 5 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "saturday",  opening_hour: 12,  closing_hour: 2 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "sunday",    opening_hour: 12,  closing_hour: 2 ).save!
 puts "Kebab Shop 2 created!"
 
 puts "Creating Kebab Shop 3"
@@ -42,11 +42,11 @@ kebab_shop.save!
 
 Review.new(kebab_shop_id: kebab_shop.id, content:"content test", rating: 1).save!
 
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "monday",    opening_hour: 120000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "tuesday",   opening_hour: 120000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "wednesday", opening_hour: 80000,  closing_hour: 240000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "thursday",  opening_hour: 120000,  closing_hour: 30000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "friday",    opening_hour: 80000,  closing_hour: 50000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "saturday",  opening_hour: 110000,  closing_hour: 220000 ).save!
-Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "sunday",    opening_hour: 110000,  closing_hour: 30000 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "monday",    opening_hour: 12,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "tuesday",   opening_hour: 12,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "wednesday", opening_hour: 8,  closing_hour: 24 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "thursday",  opening_hour: 12,  closing_hour: 3 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "friday",    opening_hour: 8,  closing_hour: 5 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "saturday",  opening_hour: 11,  closing_hour: 22 ).save!
+Schedule.new(kebab_shop_id: kebab_shop.id, weekday: "sunday",    opening_hour: 11,  closing_hour: 3 ).save!
 puts "Kebab Shop 3 created!"
