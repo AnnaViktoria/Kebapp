d9698c6442f7396bcdeac9a58da212af17b9326b
Nicolo Buzzi <nicolo@buzzi.org>
2019-08-27 15:17:16 +0200

    drop content from review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_091038) do
+ActiveRecord::Schema.define(version: 2019_08_27_131237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 2019_08_26_091038) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
-    t.string "content"
     t.bigint "kebab_shop_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
